ef0b97bcdcf918816641ffc76d19acea85adf405
Charles <charles@Charless-Mac.local>
2020-11-19 02:37:57 -0800

    =modified permutation.cpp

--- permutation.cpp
+++ permutation.cpp
@@ -6,31 +6,49 @@
 #include <iterator>
 using namespace std;
 
-vector<string> make_permutations(int length, int num) {
+vector<string> make_permutations(int length, int num){
   if (length == 0) {
     return vector<string>{};
   }
+    
   vector<string> results;
   vector<string> children = make_permutations(length - 1, num);
   vector<int> numbers;
-  stringstream s_numbers;
+  stringstream ss;
   
   for (char letter = 'a'; letter <= 'z'; letter++) { 
-    results.emplace_back(1, letter);
+    results.emplace_back(1, letter); //emplace_back() moves forward values with less time consumption
     for(auto child : children) {
-      results.push_back(std::string(1, letter) + child);
+      results.push_back(std::string(1, letter) + child); //push_back() copys values with time consumption, but safer
     }
   }
   
-  for(int i = 0; 0 <= num; i++){
-   numbers.push_back(i);
+  for(int i = 0; i < num; i++){
+   numbers.emplace_back(i);
+   for(int j = i + 1; j < num; j++){
+     numbers.push_back(j);    
+   }
   }
-  copy(numbers.begin(), numbers.end(), ostream_iterator<int>(s_numbers, ""));
+ 
+  for(auto n : numbers){
+   ss << numbers[n];
+  
+   string s_numbers = ss.str();
+  
+  results.push_back(s_numbers);
+  }
   return results;
 }
 
 int main()
 {
- auto results = make_permutations(2, 3);
-    for(auto s : results) cout << s << endl;
+ ofstream output;
+  output.open("wordlist.lst");
+ auto results = make_permutations(3, 3);
+    for(auto s : results){ 
+      cout << s << endl;
+      output << s << "\n";
+    }
+    output.close();
+ return 0;
 }
